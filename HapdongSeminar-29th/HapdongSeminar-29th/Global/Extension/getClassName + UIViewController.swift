0cf559c2d1e64b17922156a53fcc955e024d1d53
i-colours-u <i.colours.u@gmail.com>
2021-11-13 12:55:52 +0900

    [add] first project setting

---
+++ HapdongSeminar-29th/HapdongSeminar-29th/Global/Extension/getClassName + UIViewController.swift
@@ -0,0 +1,43 @@
+//
+//  getClassName + UIViewController.swift
+//  sopt_29th_Assignment
+//
+//  Created by 송지훈 on 2021/11/12.
+//
+
+import Foundation
+import UIKit
+
+/**
+
+  - Description:
+ 
+          각 VC,TVC,CVC의 className을 String으로 가져올 수 있도록 연산 프로퍼티를 설정합니다.
+          요 값들은 나중에 Identifier에 잘 써먹을 수 있습니다 ^__^
+*/
+
+
+
+
+extension UIViewController {
+    
+    static var className: String {
+        NSStringFromClass(self.classForCoder()).components(separatedBy: ".").last!
+    }
+    
+     var className: String {
+        NSStringFromClass(self.classForCoder).components(separatedBy: ".").last!
+    }
+}
+
+
+extension UIView {
+    
+    static var className: String {
+        NSStringFromClass(self.classForCoder()).components(separatedBy: ".").last!
+    }
+    
+     var className: String {
+        NSStringFromClass(self.classForCoder).components(separatedBy: ".").last!
+    }
+}
---
+++ HapdongSeminar-29th/HapdongSeminar-29th/Global/UIComponents/sampleComponents.swift
@@ -0,0 +1,8 @@
+//
+//  sampleComponents.swift
+//  HapdongSeminar-29th
+//
+//  Created by 송지훈 on 2021/11/13.
+//
+
+import Foundation
