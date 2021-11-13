232d6657d35913dcfa3f5a5d51d3423df3bdbf77
i-colours-u <i.colours.u@gmail.com>
2021-11-13 12:49:34 +0900

    [init] pod setting

---
+++ HapdongSeminar-29th/HapdongSeminar-29th/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HapdongSeminar-29th
+//
+//  Created by 송지훈 on 2021/11/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ HapdongSeminar-29th/Pods/Target Support Files/Pods-HapdongSeminar-29th/Pods-HapdongSeminar-29th-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HapdongSeminar_29th : NSObject
+@end
+@implementation PodsDummy_Pods_HapdongSeminar_29th
+@end
