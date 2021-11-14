9d53429026e9243ebeec953b4532cf6bc12dec4b
hellozo0 <ahju0011@gmail.com>
2021-11-15 03:22:10 +0900

    ✅[CHORE] : nav bar UI 완성 (#1)

--- HapdongSeminar-29th/HapdongSeminar-29th/Screens/Home/HomeVC.swift
+++ HapdongSeminar-29th/HapdongSeminar-29th/Screens/Home/HomeVC.swift
@@ -2,19 +2,36 @@
 //  HomeVC.swift
 //  HapdongSeminar-29th
 //
-//  Created by 송지훈 on 2021/11/13.
+//  Created by 안현주 on 2021/11/15.
 //
 
 import UIKit
 
 class HomeVC: UIViewController {
 
+  // MARK: - Vars & Lets Part
+    @IBOutlet weak var addressBtn: UIButton!
+    
+    
+  // MARK: - UI Component Part
+
+  
+  // MARK: - Life Cycle Part
     override func viewDidLoad() {
         super.viewDidLoad()
-
+         
     }
     
+  // MARK: - IBAction Part
+    @IBAction func touchUpToShowAddress(_ sender: Any) {
+        //나중을 위해 언젠가.. 터치를 하면 주소를 정해야하는 팝업..? 이나 주소가..
+    }
+    
+  
+  // MARK: - Custom Method Part
 
-
+  
+  // MARK: - @objc Function Part
 
 }
+// MARK: - Extension Part
