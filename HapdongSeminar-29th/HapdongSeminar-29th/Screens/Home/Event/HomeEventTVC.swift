5f396d9fd8fca8f2257529c2b303114b2bac897d
hellozo0 <ahju0011@gmail.com>
2021-11-16 03:41:17 +0900

    ✅[CHORE] : PageController 연결 (#6)

--- HapdongSeminar-29th/HapdongSeminar-29th/Screens/Home/Event/HomeEventTVC.swift
+++ HapdongSeminar-29th/HapdongSeminar-29th/Screens/Home/Event/HomeEventTVC.swift
@@ -10,11 +10,12 @@ import UIKit
 class HomeEventTVC: UITableViewCell {
 
     // MARK: - Vars & Lets Part
-    var imgName = ["event_1", "event_2", "event_3"]
-    
     static let identifier = "HomeEventTVC"
-    @IBOutlet weak var eventCollectionView: UICollectionView!
+    var imgName = ["event_1", "event_2", "event_3"]
+    @IBOutlet weak var eventImgView: UIImageView!
     @IBOutlet weak var pager: UIPageControl!
+    
+    
     
     // MARK: - UI Component Part
 
@@ -33,19 +34,33 @@ class HomeEventTVC: UITableViewCell {
     // MARK: - IBAction Part
 
     @IBAction func pageChanged(_ sender: UIPageControl) {
-        let indexPath = IndexPath(item: sender.currentPage, section: 0)
-        eventCollectionView.scrollToItem(at: indexPath, at: .centeredVertically, animated: true)
+        eventImgView.image = UIImage(named: imgName[pager.currentPage])
     }
     
     // MARK: - Custom Method Part
     func setPager() {
+        //페이지 컨트롤의 전체 페이지를 images 배열의 전체 개수 값으로 설정
         pager.numberOfPages = imgName.count
+        // 페이지 컨트롤의 현재 페이지를 0으로 설정
         pager.currentPage = 0
+        eventImgView.image = UIImage(named: imgName[0])
     }
+    
+
     
     // MARK: - @objc Function Part
 
-  }
+}
 
   // MARK: - Extension Part
-
+extension HomeEventTVC : UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let width = scrollView.bounds.size.width // 너비 저장
+        let x = scrollView.contentOffset.x + (width / 2.0) // 현재 스크롤한 x좌표 저장
+               
+        let newPage = Int(x / width)
+        if pager.currentPage != newPage {
+            pager.currentPage = newPage
+        }
+    }
+}
