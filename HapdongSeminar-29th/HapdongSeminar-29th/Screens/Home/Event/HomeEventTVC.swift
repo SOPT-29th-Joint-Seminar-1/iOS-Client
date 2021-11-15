27b8d7eb04d13bbddbd5fc4375fde1c705763f69
hellozo0 <ahju0011@gmail.com>
2021-11-16 02:09:05 +0900

    ✅[CHORE] : Event TVC레이아웃 (#6)

--- HapdongSeminar-29th/HapdongSeminar-29th/Screens/Home/Event/HomeEventTVC.swift
+++ HapdongSeminar-29th/HapdongSeminar-29th/Screens/Home/Event/HomeEventTVC.swift
@@ -10,8 +10,12 @@ import UIKit
 class HomeEventTVC: UITableViewCell {
 
     // MARK: - Vars & Lets Part
+    var imgName = ["event_1", "event_2", "event_3"]
+    
     static let identifier = "HomeEventTVC"
-
+    @IBOutlet weak var eventCollectionView: UICollectionView!
+    @IBOutlet weak var pager: UIPageControl!
+    
     // MARK: - UI Component Part
 
     
@@ -19,18 +23,25 @@ class HomeEventTVC: UITableViewCell {
 
       override func awakeFromNib() {
           super.awakeFromNib()
+          setPager()
       }
 
       override func setSelected(_ selected: Bool, animated: Bool) {
           super.setSelected(selected, animated: animated)
-
       }
       
     // MARK: - IBAction Part
 
+    @IBAction func pageChanged(_ sender: UIPageControl) {
+        let indexPath = IndexPath(item: sender.currentPage, section: 0)
+        eventCollectionView.scrollToItem(at: indexPath, at: .centeredVertically, animated: true)
+    }
     
     // MARK: - Custom Method Part
-
+    func setPager() {
+        pager.numberOfPages = imgName.count
+        pager.currentPage = 0
+    }
     
     // MARK: - @objc Function Part
 
