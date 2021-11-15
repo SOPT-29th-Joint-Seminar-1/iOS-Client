d74d0ead4754d06eb590c0110bc3fb1bd1c23f4f
hellozo0 <ahju0011@gmail.com>
2021-11-16 01:24:29 +0900

    ✅[CHORE] : xib UI수정 (#5)

--- HapdongSeminar-29th/HapdongSeminar-29th/Screens/Home/Header/HomeHeaderTVC.swift
+++ HapdongSeminar-29th/HapdongSeminar-29th/Screens/Home/Header/HomeHeaderTVC.swift
@@ -11,14 +11,18 @@ class HomeHeaderTVC: UITableViewCell {
 
     // MARK: - Vars & Lets Part
     static let identifier = "HomeHeaderTVC"
-
+    @IBOutlet weak var deliveryInfoBtn: UIButton!
+    @IBOutlet weak var firstUserBtn: UIButton!
+    @IBOutlet weak var originUserBtn: UIButton!
+    
+    
     // MARK: - UI Component Part
 
     
     // MARK: - Life Cycle Part
-
       override func awakeFromNib() {
           super.awakeFromNib()
+          setUI()
       }
 
       override func setSelected(_ selected: Bool, animated: Bool) {
@@ -27,10 +31,23 @@ class HomeHeaderTVC: UITableViewCell {
       }
       
     // MARK: - IBAction Part
-
+    @IBAction func touchUpToShowInfo(_ sender: Any) {
+        //회색 배경 깔고
+        //이미지 띄우고
+        //3초 띄위기
+        //다른곳 터치하면 꺼지기
+    }
+    
+    @IBAction func touchUpToGoGiudeView(_ sender: Any) {
+        //가이드뷰로 고고씽
+    }
+    
     
     // MARK: - Custom Method Part
-
+    func setUI(){
+        firstUserBtn.contentHorizontalAlignment = .left
+        originUserBtn.contentHorizontalAlignment = .left
+    }
     
     // MARK: - @objc Function Part
 
