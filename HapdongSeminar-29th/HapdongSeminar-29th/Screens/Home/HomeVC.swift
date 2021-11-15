fedcce611076f50b9cd144f6b58ef97b6adb68e3
hellozo0 <ahju0011@gmail.com>
2021-11-16 01:24:45 +0900

    ✅[CHORE] :  TV에 TVC등록 (#5)

--- HapdongSeminar-29th/HapdongSeminar-29th/Screens/Home/HomeVC.swift
+++ HapdongSeminar-29th/HapdongSeminar-29th/Screens/Home/HomeVC.swift
@@ -12,14 +12,15 @@ class HomeVC: UIViewController {
   // MARK: - Vars & Lets Part
     @IBOutlet weak var addressBtn: UIButton!
     @IBOutlet weak var homeTV: UITableView!
-    
-  // MARK: - UI Component Part
+
+    // MARK: - UI Component Part
 
   
   // MARK: - Life Cycle Part
     override func viewDidLoad() {
         super.viewDidLoad()
-         
+        makeDelegate()
+        registerXib()
     }
     
   // MARK: - IBAction Part
@@ -29,9 +30,80 @@ class HomeVC: UIViewController {
     
   
   // MARK: - Custom Method Part
-
+    func makeDelegate() {
+        homeTV.dataSource = self
+        homeTV.delegate = self
+    }
+    
+    func registerXib() {
+        let headerTVC = UINib(nibName: HomeHeaderTVC.identifier, bundle: nil)
+        homeTV.register(headerTVC, forCellReuseIdentifier: HomeHeaderTVC.identifier)
+        
+        let eventsTVC = UINib(nibName: HomeEventTVC.identifier, bundle: nil)
+        homeTV.register(eventsTVC, forCellReuseIdentifier: HomeEventTVC.identifier)
+        
+        let reviewTVC = UINib(nibName: HomeReviewTVC.identifier, bundle: nil)
+        homeTV.register(reviewTVC, forCellReuseIdentifier: HomeReviewTVC.identifier)
+        
+        let newsTVC = UINib(nibName: HomeNewsTVC.identifier, bundle: nil)
+        homeTV.register(newsTVC, forCellReuseIdentifier: HomeNewsTVC.identifier)
+    }
   
   // MARK: - @objc Function Part
 
 }
 // MARK: - Extension Part
+extension HomeVC: UITableViewDelegate {
+    // 높이 지정 index별
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if (indexPath.section == 0) { //HeaderView
+            return 252
+        }
+        else if (indexPath.section == 1) { //EventView
+            return 304
+        }
+        else if (indexPath.section == 2) { //ReviewView
+            return 305
+        }
+        else { //NewsView
+            return 609
+        }
+    }
+}
+
+extension HomeVC: UITableViewDataSource {
+     //한 section마다 몇개의 row를 넣어야하는지 지정하는 함수
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1 //한 섹션의 한개의 row니까
+    }
+    
+    // indexPath를 활용해 TableViewcell 별로 데이터를 다르게 지정할 수 있다.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if (indexPath.section == 0) { //HeaderView
+            guard let headerTVC = tableView.dequeueReusableCell(withIdentifier: HomeHeaderTVC.identifier) as?
+                    HomeHeaderTVC else {return UITableViewCell()}
+            
+    //        headertvc.setData(rank: indexPath.row, appData: appContentList[indexPath.row])
+            return headerTVC
+        }
+        else if (indexPath.section == 1) { //EventView
+            guard let eventTVC = tableView.dequeueReusableCell(withIdentifier: HomeEventTVC.identifier) as?
+                    HomeEventTVC else {return UITableViewCell()}
+            
+            return eventTVC
+        }
+        else if (indexPath.section == 2) { //ReviewView
+            guard let reviewTVC = tableView.dequeueReusableCell(withIdentifier: HomeReviewTVC.identifier) as?
+                    HomeReviewTVC else {return UITableViewCell()}
+            
+            return reviewTVC
+        }
+        else { //NewsView
+            guard let newsTVC = tableView.dequeueReusableCell(withIdentifier: HomeNewsTVC.identifier) as?
+                    HomeNewsTVC else {return UITableViewCell()}
+            
+            return newsTVC
+        }
+
+    }
+}
