8170239f9bb2ea182463e5014d64bb6f9bb16677
i-colours-u <i.colours.u@gmail.com>
2021-11-16 22:40:49 +0900

    :bento:[feat] ManuCellTVC 틀 완성 (#23)

---
+++ HapdongSeminar-29th/HapdongSeminar-29th/Screens/Guide/ManualCell/GuideManualTVC.swift
@@ -0,0 +1,34 @@
+//
+//  GuideManualTVC.swift
+//  HapdongSeminar-29th
+//
+//  Created by 송지훈 on 2021/11/16.
+//
+
+import UIKit
+
+class GuideManualTVC: UITableViewCell,UITableViewRegisterable {
+  
+  // MARK: - Vars & Lets Part
+  static var isFromNib = true
+  
+  
+  // MARK: - UI Component Part
+  
+  
+  // MARK: - Life Cycle Parts
+  
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+  // MARK: - Custom Method Parts
+
+}
---
+++ HapdongSeminar-29th/HapdongSeminar-29th/Screens/Guide/ManualCell/ManualStep/GuideManualStepCVC.swift
@@ -0,0 +1,17 @@
+//
+//  GuideManualStepCVC.swift
+//  HapdongSeminar-29th
+//
+//  Created by 송지훈 on 2021/11/16.
+//
+
+import UIKit
+
+class GuideManualStepCVC: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
