6afb5f4822d6a02fd9bf7ff00cfe0d54e6b7a808
i-colours-u <i.colours.u@gmail.com>
2021-11-16 11:23:50 +0900

    [chore] :moyai: create search cell xib

---
+++ HapdongSeminar-29th/HapdongSeminar-29th/Screens/Guide/SearchCell/GuideSearchTVC.swift
@@ -0,0 +1,34 @@
+//
+//  GuideSearchTVC.swift
+//  HapdongSeminar-29th
+//
+//  Created by 송지훈 on 2021/11/16.
+//
+
+import UIKit
+
+class GuideSearchTVC: UITableViewCell {
+  
+  // MARK: - Vars & Lets Part
+
+  
+  // MARK: - UI Component Part
+
+
+
+  // MARK: - Life Cycle Part
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+  
+  // MARK: - Custom Method Part
+    
+}
